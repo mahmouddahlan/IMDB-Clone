c16e822c6f14ed02754667010591b174a88dcecd
Mahmoud Dahlan <mahmoudmmsd10@gmail.com>
2024-09-29 01:45:26 -0400

    Initial commit

---
+++ src/MovieCard.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function MovieCard({Movie}) {
+    const movie = Movie;
+    return (<>
+        <div className="movie">
+            <div><p>{movie.Year}</p></div>
+            <div>
+                <img
+                    src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/404'}
+                    alt="Movie Poster"
+                />
+            </div>
+            <div>
+                <span> {movie.Type}</span>
+                <h3>{movie.Title}</h3>
+            </div>
+
+        </div>
+    </>);
+}
+
+export default MovieCard;
